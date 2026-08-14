be61116801e03be186824b56e8152b470b48c212
prafpawar11 <prafpawar11@gmail.com>
2026-08-14 08:04:35 +0530

    Anjali TC 2 code

---
+++ Demo3.js
@@ -0,0 +1,6 @@
+console.log("Anjali Automation Test Engineer has Written new Test cases");
+console.log("Anjali Automation Test Engineer has Written new Test cases");
+console.log("Anjali Automation Test Engineer has Written new Test cases");
+console.log("Anjali Automation Test Engineer has Written new Test cases");
+console.log("Anjali Automation Test Engineer has Written new Test cases");
+console.log("Anjali Automation Test Engineer has Written new Test cases");
