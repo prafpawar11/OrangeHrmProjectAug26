e706a2a790b47e501a85b1d1500fe5fd07da10ff
prafpawar11 <prafpawar11@gmail.com>
2026-08-15 08:44:43 +0530

    Anjali Automation Test Engineer - DealsPage

---
+++ DealsPage.js
@@ -0,0 +1,8 @@
+function createNewDeal(title, description, amount, expdate)
+{
+    console.log(`title is ${title}`);
+    console.log("title is Enter in title text box");
+    console.log("description is added");
+    console.log("Amount is added");
+    console.log("Deals is expired");
+}
